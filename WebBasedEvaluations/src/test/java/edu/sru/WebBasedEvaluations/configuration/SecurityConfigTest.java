11067cd0c1df66c8bd490e9a8c025a00cc4b4774
ellio <ellio@ElliotsLaptop.lan>
2024-01-31 23:49:58 -0500

    Security config test: Added necessary annotations and general code

--- WebBasedEvaluations/src/test/java/edu/sru/WebBasedEvaluations/configuration/SecurityConfigTest.java
+++ WebBasedEvaluations/src/test/java/edu/sru/WebBasedEvaluations/configuration/SecurityConfigTest.java
@@ -1,47 +1,40 @@
 package edu.sru.WebBasedEvaluations.configuration;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import edu.sru.WebBasedEvaluations.company.Company;
-import edu.sru.WebBasedEvaluations.domain.Role;
-import edu.sru.WebBasedEvaluations.domain.User;
+/*
+ * JUnit test class intended to test the functionality of SecurityConfiguration class
+ * 
+ * Authored by Elliot McIlwain
+ */
 
-@Configuration
+@SpringBootTest
+@WebMvcTest
+@EnableWebSecurity
 public class SecurityConfigTest {
+
+	@Autowired
+	private MockMvc mockMvc;
 	
-	/*
-	 * This is broken, I have no idea what is wrong with this and using MyUserDetailsService
-	 * and UserDetails
-	 *
+	@Test
+	@WithMockUser(username = "testuser", authorities = "USER")
+	public void testUnauthenticated() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.get("/"))
+			   .andExpect(MockMvcResultMatchers.status().isOk());
+	}
 	
-	@Bean
-	public InMemoryUserDetailsManager userDetailsService() {
-		String name = "test";
-		String firstName = "test";
-		String lastName = "test";
-		String email = "test";
-		String password = "test";
-		int employeeId = 69420;
-		String dateOfHire = "test";
-		String jobTitle = "test";
-		String supervisor = "test";
-		String divisionBranch = "test";
-		String deptName = "test";
-		Company co = new Company("test");
-		Role role = new Role();
-		role.setName("test");
-		boolean companySuperUser = true;
-		boolean superUser = true;
-		
-		User newUser = new User(name, firstName, lastName, email, password, employeeId, dateOfHire, jobTitle,
-				supervisor, divisionBranch, deptName, co, role, companySuperUser, superUser);
-		UserDetails user;
-		user = (UserDetails) newUser;
-		return new InMemoryUserDetailsManager(user);
-	} */
+	@Test
+	@WithMockUser(username = "admin", authorities = "ADMIN")
+	public void testAdmin() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.get("/h2-console"))
+			   .andExpect(MockMvcResultMatchers.status().isOk());
+	}
 }
