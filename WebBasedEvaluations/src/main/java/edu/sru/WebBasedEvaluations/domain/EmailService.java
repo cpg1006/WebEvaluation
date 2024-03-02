204b80266ecd777337c1364aa15d04d009710b20
brnew <brnew@10.0.0.149>
2024-03-01 21:32:38 -0500

    Updated Email service functionality

--- WebBasedEvaluations/src/main/java/edu/sru/WebBasedEvaluations/domain/EmailService.java
+++ WebBasedEvaluations/src/main/java/edu/sru/WebBasedEvaluations/domain/EmailService.java
@@ -5,19 +5,45 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
+
 @Service
 public class EmailService {
 	@Autowired
 	private JavaMailSender mailSender;
+	public static String selfEvaluationReviewEmailBody = "A self-Evaluation has been requested.";
+	public static String selfEvaluationReviewEmailSubject = "Self-Evaluation";
 	
-	public void sendEmail(String toEmail, String emailSubject, String emailBody) {
+	public static String devFromEmail = "";
+	public static String devToEmail = "";
+	
+	public static enum EmailType{
+		SELFEVALUATION
+	}
+	
+	public void sendEmail(String toEmail, String ccEmail, EmailType type) {
 		
 		SimpleMailMessage message = new SimpleMailMessage();
 		
-		message.setFrom("admin@gmail.com");
-		message.setTo(toEmail);
-		message.setText(emailBody);
-		message.setSubject(emailSubject);
+		switch(type) {
+		case SELFEVALUATION: 
+			//local testing
+			message.setFrom(devFromEmail);
+			message.setTo(devToEmail);
+			message.setCc(devToEmail);
+			
+			//release version
+//			message.setFrom("admin@gmail.com");
+//			message.setTo(toEmail);
+//			message.setCc(ccEmail);
+			
+			message.setText(selfEvaluationReviewEmailBody);
+			message.setSubject(selfEvaluationReviewEmailSubject);
+			
+		default:
+			break;
+		
+		}
+		
 		
 		mailSender.send(message);
 	}
