7b26c62f706382c75da5fa3922e61e5291f4014e
cpg1006 <cpg1006@sru.edu>
2024-03-04 16:32:02 -0500

    Check deadlines each day and add deadline 1 month from day

--- WebBasedEvaluations/src/main/java/edu/sru/WebBasedEvaluations/service/EvaluatorService.java
+++ WebBasedEvaluations/src/main/java/edu/sru/WebBasedEvaluations/service/EvaluatorService.java
@@ -1,5 +1,49 @@
 package edu.sru.WebBasedEvaluations.service;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import edu.sru.WebBasedEvaluations.domain.Evaluator;
+import edu.sru.WebBasedEvaluations.repository.EvaluatorRepository;
+import edu.sru.WebBasedEvaluations.repository.UserRepository;
+
+
+@Service
 public class EvaluatorService {
 
+	 @Autowired
+	    private EvaluatorRepository evaluatorRepository;
+	    
+	    @Autowired
+	    private UserRepository userRepository;
+	    
+	    
+	
+
+	 @Scheduled(cron = "0 30 8 * * *")
+	    @Transactional
+	    public void sendEvaluationReminders() {
+	    	 Calendar calendar = Calendar.getInstance();
+	    	  calendar.add(Calendar.MONTH, 1);
+	    	  Date newDeadline = calendar.getTime();
+
+	    	 List<Evaluator> evaluators = (List<Evaluator>) evaluatorRepository.findAll();
+	         for (Evaluator evaluator : evaluators) {
+	        	 
+	        	 if (evaluator.getDeadline() == null) {
+	        		 evaluator.setDeadline(newDeadline);
+	        		 evaluatorRepository.save(evaluator);
+	        		 System.out.println("CHANGED DEADLINE OF EVALUATOR");
+	        	 }		 
+	         }
+	 }
+
 }
+
+
